069c4fa5270bf778eb07db1d73850788cb3a6e9f
mar1o9 <144303445+mar1o9@users.noreply.github.com>
2025-10-22 01:14:59 +0300

    redefined the config to use .yml and .env file togther

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
-use std::fs;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::{Router, extract::State};
+use std::{env, error, fs, sync::Arc};
 
+use dotenv::dotenv;
+use sea_orm::DatabaseConnection;
 use serde::Deserialize;
 use tokio::{net::TcpListener, signal};
+use tracing::info;
 
-use axum::Router;
-use axum::routing::get;
-use tracing::{info, warn};
-use voice::open_db;
+pub mod entities;
+pub mod utils;
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Clone)]
+pub struct AppState {
+    db: DatabaseConnection,
+}
+
+async fn create_router(
+    config: Arc<AppConfig<'_>>,
+) -> Result<Router, Box<dyn error::Error>> {
+    let state = Arc::new(AppState {
+        db: utils::open_db(config.database_url, config.database_name).await?,
+    });
+    Ok(Router::new()
+        .route("/", get(|| async { "Hello 🚀" }))
+        .route("/_ping", get(ping_get))
+        .with_state(state))
+}
+
+async fn ping_get(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    match state.db.ping().await {
+        Ok(_) => "Healthy".to_string(),
+        Err(err) => err.to_string(),
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
 struct AppConfig<'c> {
     database_url: &'c str,
     database_name: &'c str,
@@ -18,17 +46,21 @@ struct AppConfig<'c> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = dotenv().unwrap();
     tracing_subscriber::fmt().init();
 
+    let db_url_var = env::var("DATABASE_URL")?;
+    let db_name_var = env::var("DATABASE_NAME")?;
     // Read the file content into a string
-    let json_content = fs::read_to_string("config.json")?;
-
+    let yaml_string = fs::read_to_string("config.yml")?;
+    let yml_config = yaml_string
+        .replace("${DATABASE_URL}", &db_url_var)
+        .replace("${DATABASE_NAME}", &db_name_var);
     // Deserialize the JSON string into your AppConfig struct
-    let config: AppConfig = serde_json::from_str(&json_content)?;
-    let db = open_db(config.database_url, config.database_name).await?;
-    let app = Router::new()
-        .route("/", get(|| async { "Hello 🚀" }))
-        .with_state(db);
+    let config: AppConfig = serde_yaml::from_str(yml_config.as_str())?;
+    // Deserialize the JSON string into your AppConfig struct
+    let config = Arc::new(config);
+    let app = create_router(config.clone()).await?;
     let addr =
         TcpListener::bind(format!("{}:{}", config.host, config.port)).await?;
 
@@ -36,14 +68,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Serve with graceful shutdown
     axum::serve(addr, app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
+        .await?;
     info!("Server exited gracefully");
     Ok(())
 }
 
 async fn shutdown_signal() {
-    warn!("shutting down the server");
     let ctrl_c = async {
         tokio::signal::ctrl_c()
             .await

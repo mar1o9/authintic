2d597e72b26f38ce4b0a067fca02572bd9bed085
mar1o9 <144303445+mar1o9@users.noreply.github.com>
2025-10-24 01:50:08 +0300

    modifyed the user shcema to use date time with time zone

--- migration/src/m20251010_151835_users.rs
+++ migration/src/m20251010_151835_users.rs
@@ -13,12 +13,23 @@ impl MigrationTrait for Migration {
                     .table(Users::Table)
                     .if_not_exists()
                     .col(pk_auto(Users::Id))
+                    .col(uuid_uniq(Users::Pid))
                     .col(string_uniq(Users::Username))
                     .col(string(Users::Name))
                     .col(string_uniq(Users::Email))
                     .col(string(Users::Password))
-                    .col(time(Users::CreatedAt))
-                    .col(time(Users::UpdatedAt))
+                    .col(
+                        ColumnDef::new(Users::CreatedAt)
+                            .timestamp_with_time_zone() // Example: using timestamp with time zone
+                            .not_null()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_owned()), // Default to current time
+                    )
+                    .col(
+                        ColumnDef::new(Users::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_owned()),
+                    )
                     .to_owned(),
             )
             .await
@@ -36,6 +47,7 @@ impl MigrationTrait for Migration {
 enum Users {
     Table,
     Id,
+    Pid,
     Username,
     Name,
     Email,
